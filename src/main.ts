87df50fbe3c581166c6b2defb96587686fc829c9
hm243695czl <1224311695@qq.com>
2022-12-12 19:29:47 +0800

    项目初始化

---
+++ src/main.ts
@@ -0,0 +1,55 @@
+import { createApp } from 'vue';
+import {
+	VXETable,
+	Table,
+	Icon,
+	Column,
+	Filter
+} from 'vxe-table';
+import pinia from '/@/stores/index';
+import App from './App.vue';
+import router from './router';
+import { directive } from '/@/utils/directive';
+import other from '/@/utils/other';
+
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import formCreate from '@form-create/element-ui';
+import FcDesigner from '@form-create/designer';
+import 'vxe-table/lib/style.css';
+import '/@/theme/index.scss';
+import mitt from 'mitt';
+
+const app = createApp(App);
+
+function useTable(app: any) {
+	app.use(Table).use(Icon).use(Column).use(Filter);
+}
+VXETable.setup({
+	table: {
+		stripe: true, // 斑马条纹
+		border: true, // 边框
+		align: 'center', // 文字居中
+		highlightHoverRow: true, // 鼠标移入高亮当前行
+		showOverflow: true, // 单行显示,
+		columnConfig: {
+			resizable: true, // 可拖拽列
+		},
+		filterConfig: {
+			remote: true, // 开启后端筛选
+			iconNone: 'vxe-icon-search', // 设置筛选图标
+			iconMatch: 'vxe-icon-search', // 设置筛选图标
+		},
+		sortConfig: {
+			remote: true // 开启后端排序
+		}
+	}
+});
+
+directive(app);
+other.elSvg(app);
+
+// @ts-ignore
+app.use(pinia).use(router).use(ElementPlus).use(formCreate).use(FcDesigner).use(useTable).mount('#app');
+
+app.config.globalProperties.mittBus = mitt();
---
+++ src/views/system/form-config.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="form-config-container h100">
+    <form-create v-model="value" :rule="rule" v-model:api="fApi" :option="options"/>
+  </div>
+</template>
+
+<script lang="ts">
+import {reactive, toRefs} from 'vue';
+
+export default {
+  name: 'form-config',
+  setup() {
+    const state = reactive({
+      // 实例对象
+      fApi:{},
+      // 表单数据
+      value: {
+        goods_name: '1234',
+        label: [2]
+      },
+      // 组件参数配置
+      options:{
+        onSubmit:(formData, fApi)=>{
+          console.log(formData);
+          console.log(fApi);
+        },
+        resetBtn:true
+      },
+      // 表单生成规则
+      rule:[
+        {
+          type:'input',
+          field:'goods_name',
+          title:'商品名称',
+          value:'form-create'
+        },
+        {
+          type:'checkbox',
+          field:'label',
+          title:'标签',
+          value:[0,1,2,3],
+          options: [
+            {label:'好用',value:0},
+            {label:'快速',value:1},
+            {label:'高效',value:2},
+            {label:'全能',value:3},
+          ]
+        }
+      ]
+    });
+    return {
+      ...toRefs(state)
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+  .form-config-container{
+    padding: 20px;
+    overflow: auto;
+  }
+</style>
