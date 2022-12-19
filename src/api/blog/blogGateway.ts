cb3394ae39633b4e4922a0b0c936a8d4166396b3
hm243695czl <1224311695@qq.com>
2022-12-19 16:15:34 +0800

    完成获取博客列表和类型列表

--- src/api/blog/blogGateway.ts
+++ src/api/blog/blogGateway.ts
@@ -1,3 +1,4 @@
 export const blogGatewayBaseApi = '/gateway/';
 
 export const getBlogListApi = `${blogGatewayBaseApi}getBlogList`;
+export const getGatewayTypeList = `${blogGatewayBaseApi}getTypeList`;
--- src/views/gatewayBlog/home/index.vue
+++ src/views/gatewayBlog/home/index.vue
@@ -2,7 +2,54 @@
 	<div class='blog-home-container'>
 		<BlogHeader />
 		<div class='home-content base-width'>
-			博客首页
+			<el-row :gutter='20'>
+				<el-col :span='16'>
+					<el-card shadow='never'>
+						<template #header>
+							<div class="card-header card-header-blog flex flex-space-between flex-align-center">
+								<div class='card-header-left'>博客</div>
+								<div class='card-header-right'>
+									共<span>{{pageInfo.totalRecords}}</span>篇
+								</div>
+							</div>
+						</template>
+						<div class='info flex flex-space-between flex-align-center' v-for='item in blogList' :key='item.id'>
+							<div class='content'>
+								<div class='title'>{{item.title}}</div>
+								<div class='summary'>{{item.summary}}</div>
+								<div class='content-footer flex flex-space-between flex-align-center'>
+									<div class='publish-date flex flex-start flex-align-center'>
+										<el-icon><ele-Clock /></el-icon>{{item.addTime.slice(0, 10)}}
+										<el-icon class='view-icon'><ele-View /></el-icon>{{item.views}}
+									</div>
+									<el-tag>{{item.typeName}}</el-tag>
+								</div>
+							</div>
+							<div class='img'>
+								<img :src='item.pictureUrl' alt=''>
+							</div>
+						</div>
+					</el-card>
+				</el-col>
+				<el-col :span='8'>
+					<el-card shadow='never'>
+						<template #header>
+							<div class="card-header card-header-classify flex flex-space-between flex-align-center">
+								<div class='card-header-left'>分类</div>
+								<div class='card-header-right'>
+									更多
+								</div>
+							</div>
+						</template>
+						<div class='type-list'>
+							<div class='list-item flex flex-space-between flex-align-center' v-for='item in typeList.slice(0, 6)' :key='item.id'>
+								{{item.name}}
+								<div class='type-number hl-tag'>{{item.number}}</div>
+							</div>
+						</div>
+					</el-card>
+				</el-col>
+			</el-row>
 			<PaginationCommon
 				:page-info='pageInfo'
 				@changePageSize='changePageSize'
@@ -15,8 +62,8 @@
 <script lang='ts'>
 import { defineComponent, onMounted, reactive, toRefs } from 'vue';
 import BlogHeader from '/@/components/BlogHeader/index.vue'
-import { postAction } from '/@/api/common';
-import { getBlogListApi } from '/@/api/blog/blogGateway';
+import { postAction, getAction } from '/@/api/common';
+import { getBlogListApi, getGatewayTypeList } from '/@/api/blog/blogGateway';
 import { StatusEnum } from '/@/common/status.enum';
 import PaginationCommon from '/@/components/PaginationCommon/index.vue';
 import { PageEntity } from '/@/common/page.entity';
@@ -30,6 +77,7 @@ export default defineComponent({
 	setup() {
 		const state = reactive({
 			blogList: [],
+			typeList: [],
 			pageInfo: new PageEntity()
 		});
 		const getBlogList = () => {
@@ -44,13 +92,24 @@ export default defineComponent({
 			})
 		}
 		const changePageSize = (size: number) => {
-
+			state.pageInfo.pageSize = size;
+			state.pageInfo.pageIndex = 1;
+			getBlogList();
 		};
 		const changePageIndex = (index: number) => {
-
+			state.pageInfo.pageIndex = index;
+			getBlogList();
+		};
+		const getTypeList = () => {
+			getAction(getGatewayTypeList, '', false).then(res => {
+				if (res.status === StatusEnum.SUCCESS) {
+					state.typeList = res.data;
+				}
+			});
 		};
 		onMounted(() => {
 			getBlogList();
+			getTypeList();
 		});
 		return {
 			...toRefs(state),
@@ -67,7 +126,97 @@ export default defineComponent({
 		height: 100%;
 		background: var(--el-color-white);
 		.home-content{
-			margin: 0 auto;
+			margin: 30px auto;
+			.card-header-blog{
+				.card-header-left{
+					color: #00b5ad;
+					font-weight: 700;
+					font-size: 18px;
+				}
+				.card-header-right{
+					span{
+						color: #f2711c;
+						font-size: 22px;
+					}
+				}
+			}
+			.info{
+				padding-bottom: 20px;
+				border-bottom: 1px dashed #ccc;
+				padding-top: 20px;
+				&:first-child{
+					padding-top: 0;
+				}
+				.content{
+					flex: 1;
+					padding-right: 20px;
+					.title{
+						color: #373737;
+						font-size: 20px;
+						font-weight: 700;
+						cursor: pointer;
+					}
+					.summary{
+						padding: 10px 0;
+					}
+					.content-footer{
+						.publish-date{
+							.el-icon{
+								margin-right: 5px;
+							}
+							.view-icon{
+								margin-left: 20px;
+								margin-right: 5px;
+							}
+						}
+					}
+				}
+				.img{
+					width: 180px;
+					height: 100px;
+					img{
+						width: 100%;
+						height: 100%;
+					}
+				}
+			}
+			.type-list{
+				border: 1px dashed #ccc;
+				border-radius: 5px;
+				.list-item{
+					padding: 10px;
+					border-bottom: 1px solid #ccc;
+					&:last-child{
+						border: none;
+					}
+					&:hover{
+						background: #f5f5f5;
+						cursor: pointer;
+					}
+				}
+			}
+			.hl-tag{
+				background: #fff;
+				border: 1px solid #00b5ad;
+				color: #00b5ad;
+				padding: 5px 12px;
+				border-radius: 3px;
+				position: relative;
+				&::before{
+					content: '';
+					background: #fff;
+					border-color: #00b5ad;
+					border-width: 0 0 1px 1px;
+					border-style: solid;
+					position: absolute;
+					width: 8px;
+					height: 8px;
+					transform: translateX(-50%) translateY(-50%) rotate(45deg);
+					top: 50%;
+					left: -1px;
+					z-index: 2;
+				}
+			}
 		}
 	}
 </style>
