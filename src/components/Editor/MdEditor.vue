4fa79d4ff98604c1edbda0dcbf54ea4dbd373c17
hm243695czl <1224311695@qq.com>
2022-12-29 16:55:41 +0800

    md编辑器配置优化

--- src/components/Editor/MdEditor.vue
+++ src/components/Editor/MdEditor.vue
@@ -3,16 +3,23 @@
 		<v-md-editor @change='editorChange'
 								 @blur='editorBlur'
 								 v-model='text'
+								 left-toolbar='undo redo clear | emoji | h bold italic strikethrough quote | ul ol table hr | link image code | save'
+								 :disabled-menus='[]'
 								 :height='props.height'
+								 @upload-image='updateImg'
 								 placeholder='请输入'
 								 :mode='props.mode'></v-md-editor>
 	</div>
 </template>
 
 <script lang='ts' setup>
-import { computed, reactive } from 'vue';
+import { computed, onMounted, reactive } from 'vue';
+import { getAction, uploadAction } from '/@/api/common';
+import { StatusEnum } from '/@/common/status.enum';
+import { policyApi } from '/@/api/oss';
+import { ElMessage } from 'element-plus';
 
-	const props = defineProps({
+const props = defineProps({
 		content: {
 			type: String
 		},
@@ -32,7 +39,15 @@ import { computed, reactive } from 'vue';
 
 	const state = reactive({
 		mdText: '',
-		editorText: ''
+		editorText: '',
+		dataObj: {
+			policy: '',
+			signature: '',
+			key: '',
+			ossAccessKeyId: '',
+			dir: '',
+			host: ''
+		},
 	});
 
 	const text = computed({
@@ -48,5 +63,46 @@ import { computed, reactive } from 'vue';
 	};
 	const editorBlur = () => {
 		emits('editorBlur', state.editorText);
-	}
+	};
+	const getPolicy = () => {
+		return new Promise(((resolve, reject) => {
+			getAction(policyApi, '').then(res => {
+				if (res.status === StatusEnum.SUCCESS) {
+					const { accessKeyId, dir, host, policy, signature } = res.data;
+					state.dataObj.policy = policy;
+					state.dataObj.signature = signature;
+					state.dataObj.key = dir + '/${filename}';
+					state.dataObj.ossAccessKeyId = accessKeyId;
+					state.dataObj.dir = dir;
+					state.dataObj.host = host;
+				}
+			}).catch(() => {
+				reject(false);
+			})
+		}))
+	};
+	const updateImg = (event, insertImage, files) => {
+		const formData = new FormData();
+		formData.append('policy', state.dataObj.policy);
+		formData.append('signature', state.dataObj.signature);
+		formData.append('key', state.dataObj.key);
+		formData.append('ossAccessKeyId', state.dataObj.ossAccessKeyId);
+		formData.append('dir', state.dataObj.dir);
+		formData.append('host', state.dataObj.host);
+		formData.append('file', files[0]);
+		uploadAction(state.dataObj.host, formData).then(res => {
+			if (res === '') {
+				const url = state.dataObj.host + '/' + state.dataObj.dir + '/' + files[0].name;
+				insertImage({
+					url,
+					desc: files[0].name,
+				});
+			} else {
+				ElMessage.error('上传图片失败');
+			}
+		})
+	};
+	onMounted(() => {
+		getPolicy();
+	});
 </script>
